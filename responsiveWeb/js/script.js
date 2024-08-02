e667201f298aaa5c1459fd14689e6f93991057eb
EChlarson <chl20001@byui.edu>
2024-08-01 20:38:47 -0500

    projects css and js

--- responsiveWeb/js/script.js
+++ responsiveWeb/js/script.js
@@ -1,10 +1,34 @@
-//Sticky Navigation Bar
+//Sticky Navbar
 const navbar = document.querySelector('.navbar')
 
-window.addEventListener("scroll",()=>{
+window.addEventListener('scroll',()=>{
     if(window.scrollY > 0){
         navbar.classList.add('sticky')
     }else{
         navbar.classList.remove('sticky')
     }
 })
+//End of Navbar
+
+//Projects
+const filterLinks = document.querySelectorAll('.filter-nav-link')
+
+filterLinks.forEach((filterLink)=>{
+    filterLink.addEventListener('click',(e)=>{
+        e.preventDefault()
+
+        document.querySelector(".filter-nav-link.active").classList.remove("active")
+        filterLink.classList.add("active")
+
+        const projects = document.querySelectorAll(".projects")
+        projects.forEach((project)=>{
+            project.classList.add('hide')
+        })
+
+        if(filterLink.getAttribute("date-type") === project.getAttribute("data-type")|| filterLink.getAttribute("data-type")==="all"){
+            project.classList.remove("hide")
+        }
+
+    })
+})
+//End of Projects
