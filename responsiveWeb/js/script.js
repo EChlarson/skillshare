7db5028a057f4f8ad91409c9f98a98c8f9e9c90c
EChlarson <chl20001@byui.edu>
2024-08-10 21:38:13 -0500

    video js update

--- responsiveWeb/js/script.js
+++ responsiveWeb/js/script.js
@@ -37,7 +37,47 @@ filterLinks.forEach((filterLink)=>{
 const videoContainer = document.querySelector(".video-container")
 const mainVideo = document.querySelector("video")
 const playPauseBtn = document.querySelector(".play-pause i")
+const progressBar = document.querySelector(".progress-bar")
+const skipBackward = document.querySelector(".skip-backward i")
+const skipForward = document.querySelector(".skip-forward i")
+const volumeBtn = document.querySelector(".volume i")
+const volumeSlider = document.querySelector(".left input")
+const speedBtn = document.querySelector(".playback-speed span")
+const speedOptions = document.querySelector(".speed-options")
+const speedOptionsItems = document.querySelectorAll(".speed-options div")
+const picInPicBtn = document.querySelector(".pic-in-pic span")
+const fullscreenBtn = document.querySelector(".fullscreen i")
 
+    //Progress Bar
+mainVideo.addEventListener("timeupdate", (e)=>{
+    let{currentTime,duration}=e.target
+    let percent = (currentTime/duration)*100
+    progressBar.style.width=`${percent}%`
+})
+
+    //Volume Button and Slider
+volumeBtn.addEventListener("click",()=>{
+    if(!volumeBtn.classList.contains("fa-volume-high")){
+        mainVideo.volume =0.5
+        volumeBtn.classList.replace("fa-volume-xmark","fa-volume-high")
+    } else {
+        mainVideo.volume = 0.0
+        volumeBtn.classList.replace("fa-volume-high","fa-volume-xmark")
+    }
+})
+
+volumeSlider.addEventListener("input", (e)=>{
+    mainVideo.volume = e.target.value*1
+    if(mainVideo.volume === 0) {
+        volumeBtn.classList.replace("fa-volume-high","fa-volume-xmark")
+    }else{
+        volumeBtn.classList.replace("fa-volume-xmark","fa-volume-high")
+    }
+
+    volumeSlider.value = mainVideo.volume
+})
+
+    //Play and Pause Buttons
 playPauseBtn.addEventListener("click", ()=>{
     if(mainVideo.paused){
         mainVideo.play()
@@ -45,4 +85,51 @@ playPauseBtn.addEventListener("click", ()=>{
         mainVideo.pause()
     }
 })
+
+mainVideo.addEventListener("play", ()=>{
+    playPauseBtn.classList.replace("fa-play", "fa-pause")
+})
+
+mainVideo.addEventListener("pause", ()=>{
+    playPauseBtn.classList.replace("fa-pause", "fa-play")
+})
+
+    //Skip Backward and Forward Buttons
+skipBackward.addEventListener("click",()=>{
+    mainVideo.currentTime -= 5 
+})
+
+skipForward.addEventListener("click",()=>{
+    mainVideo.currentTime += 5 
+})
+
+    //Speed Options
+speedBtn.addEventListener("click",()=>{
+    speedOptions.classList.toggle("show")
+})
+
+document.addEventListener("click",(e)=>{
+    if(e.target.tagName !== "SPAN" || e.target.className !== "material-symbols-rounded"){
+        speedOptions.classList.remove("show")
+    }
+})
+
+speedOptionsItems.forEach((option)=>{
+    option.addEventListener("click", ()=>{
+        mainVideo.playbackRate = option.dataset.speed
+        speedOptions.querySelector(".active-option").classList.remove("active-option")
+        option.classList.add("active-option")
+    })
+})
+
+    //Pic in Pic Button
+picInPicBtn.addEventListener("click",()=>{
+    mainVideo.requestPictureInPicture()
+})
+
+    //Fullscreen
+    fullscreenBtn.addEventListener("click",()=>{
+        mainVideo.requestFullscreen()
+    })
+
 //End of Video
